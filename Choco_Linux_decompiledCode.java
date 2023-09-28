0a750126d05478a4352c8dcb7d8f01f968beb722
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-09-28 10:31:33 -0700

    Add files via upload

---
+++ Choco_Linux_decompiledCode.java
@@ -0,0 +1,34 @@
+uwp_hardcode(:0E2122222222222222222222222222222222222222222222222EACCD45FFFAD3:);
+uwp_hardcode(:0E21222222222222244826FFF22222222222222222222222222EACCD45FFFAD3:);
+uwp_hardcode(:0E2122222222222222222222222222222222226922222222222EACCD45FFFAD3:);
+ 
+
+structure:"bootlinux.efi"=1111111111 
+bcd:"bootlinux.efi" =1111111111
+install:"/data/1/" =1111111111
+
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E ]"ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
+ROM_registry:"/registry/ROM/registry_data:HARDCODED:00000E"
