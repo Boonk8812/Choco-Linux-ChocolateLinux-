e2f1f1ac1b7c0a54038c7bd4406fa388cbd286be
Boonk3312 <111024718+Boonk8812@users.noreply.github.com>
2023-10-13 10:39:05 -0700

    Create CONTROLPANEL.java

---
+++ registry/CONTROLPANEL.java
@@ -0,0 +1,13 @@
+@ControlPanelValues
+{
+ LinuxVersion("type":[CUSTOMREGISTRY],}/globaldevice/KernelCONNECT/CONDRV - con/con(::00288ec-4832-cffe73)) { String="5.6.15-34.4"
+
+  condrv.sys("/system/systemLinux1/conData/condrv.sys");
+     ^ if (BugCheckDectected == 1) then echo @off %%i * error_code; $PAREMEATERS #CONADDRESS
+
+       $$i = 008827F (1 & if BugCheckCHKDSKFailed == 1) then 800FF2 is TRACK_0x0000000000001
+
+    
+  }
+
+}
